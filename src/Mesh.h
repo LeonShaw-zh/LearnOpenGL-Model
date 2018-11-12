6bd00447a2eec1ac33919d6b7d5426b9cb37f3d9
LeonShaw <LeonShaw.zh@gmail.com>
2018-11-12 11:00:26 +0800

    initialize

---
+++ src/Mesh.h
@@ -0,0 +1,38 @@
+#ifndef MESH_H
+#define MESH_H
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "Shader.h"
+
+using namespace std;
+
+struct Vertex{
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture{
+    unsigned int id;
+    string type;
+};
+
+class Mesh{
+    public:
+        vector<Vertex> vertices;
+        vector<unsigned int> indices;
+        vector<Texture> textures;
+        Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+        void Draw(Shader shader);
+    private:
+        unsigned int VAO, VBO, EBO;
+        void setupMesh();
+};
+
+#endif
